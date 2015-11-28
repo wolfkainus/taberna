ab19e1abcb94012ec3066262abf5b5ce07c7db7c
Eduardo Cornejo <edo.cornejo.m@gmail.com>
2015-11-28 15:47:08 -0300

    validate beer camps

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -2,4 +2,10 @@ class Beer < ActiveRecord::Base
   belongs_to :location
   has_many :location_beer
   has_many :location, through: :location_beer
+
+  validates :trademark, presence: true, uniqueness: true
+  validates :type, presence: true
+  validates :review, presence: true
+  validates :price, presence: true
+   
 end
